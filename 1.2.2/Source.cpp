1141aaeb5f51d7727aabd5c99959fdde815e91a2
dart <44dart44@mail.ru>
2018-04-28 22:22:21 +0300

    Добавьте файлы проекта.

---
+++ 1.2.2/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x;
+	x = 1023;
+	cout << "Эта программа выводит значение переменной x;";
+	cout << x; 
+	system("pause");
+	return 0;
+}
